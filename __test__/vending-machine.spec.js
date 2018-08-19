6d87d1f6cb59fecf54971f97f0c177897ed2379a
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-08-19 14:03:28 -0700

    printing inventory test passed

--- __test__/vending-machine.spec.js
+++ __test__/vending-machine.spec.js
@@ -1,2 +1,44 @@
-const processData = require("../src/vending-machine")
+const VendingMachine = require("../src/vending-machine.js")
 
+let vendingMachine = new VendingMachine();
+
+
+describe('Vending Machine', () => {
+    // printing inventory
+    describe('when the user ask to see inventory ', () => {
+        it('Should print a list of the inventory with all the items and details', () => {
+            expect(vendingMachine.printingInventory()).toEqual({
+                Pepsi: { stock: 5 },
+                Snickers: { stock: 2 },
+                'Ice Coffee': { stock: 3 },
+                'Ice Tea': { stock: 0 },
+                Peanuts: { stock: 8 },
+                Juice: { stock: 6 }
+            })
+        })
+    })
+    // refilling inventory
+    describe('when refill the inventory ', () => {
+        it('Should ', () => {
+            expect().toEqual()
+        })
+    })
+    // re-supplying change
+    describe('when refill the inventory ', () => {
+        it('Should ', () => {
+            expect().toEqual()
+        })
+    })
+    // dispensing inventory based on payment
+    describe('when refill the inventory ', () => {
+        it('Should ', () => {
+            expect().toEqual()
+        })
+    })
+    // returning change as coins (eg. $0.35 is 1 quarter and 1 dime)
+    describe('when refill the inventory ', () => {
+        it('Should ', () => {
+            expect().toEqual()
+        })
+    }) 
+})
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -1,4 +1,77 @@
-module.exports = data => {
+class VendingMachine{
+    constructor(inventory) {
+        this.inventory = [
+            {
+                itemName: "Pepsi",
+                price: 1.25,
+                stock: 5
+            },
+            {   
+                itemName: "Snickers",
+                price: 1.5,
+                stock: 2
+            },
+            {
+                itemName: "Ice Coffee",
+                price: 1.0,
+                stock: 3
+            },
+            {
+                itemName: "Ice Tea",
+                price: 2.0,
+                stock: 0
+            },
+            {
+                itemName: "Peanuts",
+                price: 1.1,
+                stock: 8
+            },
+            {
+                itemName: "Juice",
+                price: 3.0,
+                stock: 6
+            }
+          ];
+        this.change = [
+            {
+                name: "toonie",
+                count: 10,
+                value: 2.0
+              },
+              {
+                name: "loonie",
+                count: 10,
+                value: 1.0
+              },
+              {
+                name: "quarter",
+                count: 10,
+                value: 0.25
+              },
+              {
+                name: "dime",
+                count: 10,
+                value: 0.1
+              },
+              {
+                name: "nickel",
+                count: 10,
+                value: 0.05
+              }
+        ]
+    }
 
-    
+    printingInventory() {
+        let i = 0
+        let inventory = {}
+        for (i = 0; i < this.inventory.length; i++) {
+            let name = this.inventory[i].itemName;
+            inventory[name] = { stock: this.inventory[i].stock}
+        }
+        // console.log(inventory)
+        return inventory
+    }
 }
+
+module.exports = VendingMachine;
+
