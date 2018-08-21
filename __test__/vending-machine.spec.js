c98aa5c77c9a672e2c1485ded3889bf123d52468
Abdullah Toumeh <abdoullah.tomeh@gmail.com>
2018-08-21 03:18:38 -0700

    adding the purchase function with returning change and testing

--- __test__/vending-machine.spec.js
+++ __test__/vending-machine.spec.js
@@ -26,6 +26,12 @@ describe('Vending Machine', () => {
                 stock: 12,
             })
         })
+        it('Should return error if the item is not excits ', () => {
+            expect(vendingMachine.refillInventory('kider', 7)).toEqual('the item is not exist')
+        })
+        it('Should return error if the amount of items is not a positive numbver ', () => {
+            expect(vendingMachine.refillInventory('Pepsi', -1)).toEqual('please enter a valid value')
+        })
     })
     // re-supplying change
     describe('when refill the inventory ', () => {
@@ -33,6 +39,38 @@ describe('Vending Machine', () => {
             expect(vendingMachine.resuplychange('loonie', 10)).toEqual({ name: 'loonie', count: 20 })
         })
     })
-  
+    // dispensing inventory based on payment
+    describe('when making purchase and need to give change back ', () => {
+        it('Should dispense the required product and give the change back  ', () => {
+            expect(vendingMachine.purchase('Pepsi', 10)).toEqual({ 
+                change: { toonie: 4, quarter: 3 },
+                leftCoins: { toonie: 6, loonie: 20, quarter: 7, dime: 10, nickel: 10 },
+                product: { name: 'Pepsi' } 
+            })
+        })
+        it('Should dispense the item and return no change and return the left coins in the machine ', () => {
+            expect(vendingMachine.purchase('Pepsi', 1.25)).toEqual({
+                "change": null, 
+                "leftCoins": {"dime": 10, "loonie": 20, "nickel": 10, "quarter": 7, "toonie": 6}, 
+                "product": {"name": "Pepsi"}
+            })
+        })
+        it('Should throw an error if the item is not exist ', () => {
+            expect(vendingMachine.purchase('kinder', 10)).toEqual('the item is not excisting')
+        })
+        it('Should throw an error if the item is not a string ', () => {
+            expect(vendingMachine.purchase( 1 , 10)).toEqual('Please enter a valid value')
+        })
+        it('Should throw an error if the money is not a number ', () => {
+            expect(vendingMachine.purchase( 1 , 'loonie')).toEqual('Please enter a valid value')
+        })
+        it('Should ask to enter more money when the customer does not enter enough money ', () => {
+            expect(vendingMachine.purchase( 'Pepsi' , 1)).toEqual('money is not enough to complete the purchase')
+        })
+        it('Should say that we do not have the item any more if it is not existence anymore ', () => {
+            expect(vendingMachine.purchase( 'Ice Tea' , 2)).toEqual('we seems that we are out of this item, choose another one')
+        })
+
+    })
 
 })
--- src/vending-machine.js
+++ src/vending-machine.js
@@ -88,7 +88,6 @@ class VendingMachine{
         refilledItem.name = result.name;
         refilledItem.stock = result.stock;
         return refilledItem
-        // console.log(refilledItem)
     }
 
     resuplychange(coinName, amount) {
@@ -110,6 +109,84 @@ class VendingMachine{
         return refilledCoin
     }
 
+    purchase(purchasedProduct, money) {
+        let purchase = {
+            change: null,
+            leftCoins: {},
+            product: {}
+        };
+        if (typeof purchasedProduct !=='string') {
+            return 'Please enter a valid value'
+        }
+        if (this.inventory.find(item => item.name === purchasedProduct) === undefined) {
+            return 'the item is not excisting'
+        }
+        if (typeof money !== 'number') {
+            return 'please enter a valid money number'
+        }
+
+        let item = this.inventory.find(product => product.name === purchasedProduct)
+
+        if (item.stock === 0) {
+            return 'we seems that we are out of this item, choose another one'
+        }
+
+        if (money < item.price) {
+            return 'money is not enough to complete the purchase'
+        }
+
+        if (money === item.price ) {
+            let i = 0;
+            for(i = 0; i < this.change.length; i++) {
+                let coins = this.change[i];
+                purchase.leftCoins[coins.name] = coins.count
+            }
+            purchase.change = null;
+            purchase.product = { name: purchasedProduct}
+
+            return purchase
+        }
+
+        if (money > item.price) {
+            let changeReturnedValue = money - item.price;
+            changeReturnedValue = changeReturnedValue.toFixed(2)
+            // console.log(changeReturnedValue)8.75
+
+            this.change.reduce((accumulator, currentCoin) => {
+                let coinsGiven= 0;
+                while (changeReturnedValue >= currentCoin.value && currentCoin.count > 0) {
+                    coinsGiven++;
+                    currentCoin.count--;
+                    changeReturnedValue = changeReturnedValue - currentCoin.value;
+                    changeReturnedValue = changeReturnedValue.toFixed(2)
+                }
+                if (coinsGiven > 0) {
+                    accumulator[currentCoin.name] = coinsGiven;
+                    coinsGiven = 0;
+                }
+                purchase.change = accumulator;
+                return accumulator;
+            }, {})
+
+
+            if (changeReturnedValue > 0) {
+                return ' enter smaller money there is no coins in the machine to give you the change'
+            }
+
+            // show what coins still in the machine
+            let i = 0;
+            for (i = 0 ; i < this.change.length; i++) {
+                let coins = this.change[i]
+                purchase.leftCoins[coins.name] = coins.count;
+            }
+            purchase.leftCoins;
+
+            // show the product we ask
+            purchase.product = { name: purchasedProduct }
+
+            return purchase
+        }
+    }
 }
 
 module.exports = VendingMachine;
